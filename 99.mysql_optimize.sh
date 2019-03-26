f11600098e132c2efb17f0b8701bb41100d3a3a9
桃园令 <whymaths@gmail.com>
2019-03-26 11:45:51 +0800

    Update 99.mysql_optimize.sh

--- 99.mysql_optimize.sh
+++ 99.mysql_optimize.sh
@@ -7,6 +7,7 @@
             echo $fragmentation | grep -E '^\.' >/dev/null && fragmentation="0${fragmentation}"
             fragmentation=$(echo $fragmentation | sed 's/\(.[0-9]*\)$//g')
             printf "%-40s(%20s)\t is %04s" "$database.$name" $rows $fragmentation
+            echo "% fragmented."
             if [ ${fragmentation} -lt 100 ]; then
                 echo
                 echo "    begin to optimize $database.$name";
